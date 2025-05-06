68443ea35c9eea8cfd5891cf9b1a6747d9c390c9
arifrahmant0 <arifrahmanto667@gmail.com>
2025-05-06 21:15:21 +0700

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -9,11 +9,11 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/fontawesome-free/css/all.min.css'); ?>">
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css'); ?>">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?= base_url('assets/adminlte/dist/css/adminlte.min.css'); ?>">
 </head>
 
 <body class="hold-transition login-page">
@@ -28,7 +28,7 @@
         <?php if ($this->session->flashdata('error')): ?>
           <p style="color:red;"><?= $this->session->flashdata('error'); ?></p>
         <?php endif; ?>
-        <form action="../../index3.html" method="post">
+        <form action="<?= site_url('auth/process_login') ?>" method="post">
           <div class="input-group mb-3">
             <input type="email" class="form-control" placeholder="Email">
             <div class="input-group-append">
@@ -86,11 +86,11 @@
   <!-- /.login-box -->
 
   <!-- jQuery -->
-  <script src="../../plugins/jquery/jquery.min.js"></script>
+  <script src="<?= base_url('assets/adminlte/plugins/jquery/jquery.min.js'); ?>"></script>
   <!-- Bootstrap 4 -->
-  <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= base_url('assets/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
   <!-- AdminLTE App -->
-  <script src="../../dist/js/adminlte.min.js"></script>
+  <script src="<?= base_url('assets/adminlte/dist/js/adminlte.min.js'); ?>"></script>
 </body>
 
 </html>
