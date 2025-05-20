6ab3a95a11573ddf7be56d02988df460cf12c43a
arifrahmant0 <arifrahmanto667@gmail.com>
2025-05-20 21:55:54 +0700

    Update dashboard.php

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -1,9 +1,13 @@
 <?php
-defined(('BASEPATH')) OR exit('No Direct script access allowed');
+defined(('BASEPATH')) or exit('No Direct script access allowed');
 
-class Dashboard extends CI_Controller{
-    public function index(){
-        $data['content']='<h1>Welcome to Admin Lte 3</h1>';
+class Dashboard extends CI_Controller
+{
+    public function index()
+    {
+        $data['content'] = '<h1>Welcome to Admin Lte 3</h1>';
+        $this->load->view('templates/header');
         $this->load->view('templates/blank', $data);
+        $this->load->view('templates/footer');
     }
 }
