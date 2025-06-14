a796c90579c702371bb26d103adc2ade30b08f3e
arifrahmant0 <arifrahmanto667@gmail.com>
2025-06-15 01:55:23 +0700

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,19 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>AdminLTE 3 | Log in</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/fontawesome-free/css/all.min.css'); ?>">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- icheck bootstrap -->
   <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css'); ?>">
   <!-- Theme style -->
   <link rel="stylesheet" href="<?= base_url('assets/adminlte/dist/css/adminlte.min.css'); ?>">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 
 <body class="hold-transition login-page">
@@ -26,11 +30,11 @@
       <div class="card-body login-card-body">
         <p class="login-box-msg">Sign in to start your session</p>
         <?php if ($this->session->flashdata('error')): ?>
-          <p style="color:red;"><?= $this->session->flashdata('error'); ?></p>
+          <p style="color:red;"> <?= $this->session->flashdata('error'); ?> </p>
         <?php endif; ?>
         <form action="<?= site_url('auth/process_login') ?>" method="post">
           <div class="input-group mb-3">
-            <input type="text" name="username" class="form-control" placeholder="Email">
+            <input type="text" class="form-control" name="username" placeholder="Email">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-envelope"></span>
@@ -38,7 +42,7 @@
             </div>
           </div>
           <div class="input-group mb-3">
-            <input type="password" name="password" class="form-control" placeholder="Password">
+            <input type="password" class="form-control" name="password" placeholder="Password">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-lock"></span>
@@ -86,11 +90,12 @@
   <!-- /.login-box -->
 
   <!-- jQuery -->
-  <script src="<?= base_url('assets/adminlte/plugins/jquery/jquery.min.js'); ?>"></script>
+  <script src="<?= base_url('assets/adminlte/plugins/jquery/jquery.min.js') ?>"></script>
   <!-- Bootstrap 4 -->
-  <script src="<?= base_url('assets/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+  <script src="<?= base_url('assets/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
   <!-- AdminLTE App -->
-  <script src="<?= base_url('assets/adminlte/dist/js/adminlte.min.js'); ?>"></script>
+  <script src="<?= base_url('assets/adminlte/dist/js/adminlte.min.js') ?>"></script>
+
 </body>
 
 </html>
