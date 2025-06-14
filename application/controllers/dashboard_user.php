6a900a6390679d89c937a3c674a53d952f464788
arifrahmant0 <arifrahmanto667@gmail.com>
2025-06-15 01:55:11 +0700

    Create dashboard_user.php

---
+++ application/controllers/dashboard_user.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard_user extends MY_Controller
+{
+    public function index()
+    {
+        $data['content'] = '<h1> Welcome to Adminlte 3 in codeigniter 3</h1>';
+        $this->load->view('templates/header');
+        $this->load->view('dashboard_user', $data);
+        $this->load->view('templates/footer');
+    }
+}
